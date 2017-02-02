4c9d4761e10b5806110fc24be436cce1296743ac
darkfenix <nikrogoz@gmail.com>
2017-02-02 21:48:04 +0500

    home

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" href="css/style.css">
+    <title>My Blog</title>
+</head>
+
+<body>
+    <div class="main-wrapper">
+        <header>
+            <div class="header-top">
+                <div class="conteiner">
+                    <div class="email">
+                        <p>E-mail: <span>example@example.com</span></p>
+                    </div>
+                </div>
+            </div>
+            <div class="header-bot">
+                <div class="conteiner wrapper-flex">
+                    <div class="menu-item"><a href="#">Главная</a></div>
+                    <div class="menu-item"><a href="#">Вход</a></div>
+                </div>
+            </div>
+        </header>
+
+        <div class="header-blog">
+            <div class="conteiner wrapper-flex">
+                <div class="span1">
+                    <H1>Заголовок</H1>
+                </div>
+            </div>
+        </div>
+        <div class="conteiner-content">
+            <div class="span60">&nbsp;</div>
+            <div class="content-wrapper">
+                
+                <div class="article-preview-conteiner">
+                    <div><img class="article-picture" src="img/1.jpg" alt=""></div>
+                    <div class="container-article-head">
+                        <div class="article-date"><p>2 FEV</p></div>
+                        <div class="article-head"><p>fdgsdfgdfsgsdfgsdfg</p></div>
+                        <div class="article-preview-text">
+                            <p>Morbi vel nunc ut felis fermentum eleifend. Nullam ut porta metus. Curabitur varius elementum nunc, et ultrices sem tincidunt ac. Phasellus feugiat lobortis sapien, at condimentum neque auctor sed. Donec consectetur quam sit amet orci pharetra sit amet pharetra purus interdum.</p>
+                        </div>
+                    </div>
+                    <div class="article-block-like"></div>
+                    <div class="divider"></div>
+                </div>
+
+                <div class="side-bar">
+                    <div class="content-side-bar">Cras sit amet nibh libero, in gravida nulla.</div>
+                </div> 
+            </div>
+                       
+        </div>
+
+    </div>
+
+</body>
+
+</html>
